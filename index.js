c1d0d3fe681bf522bd1589aef861ff8f5e4955a3
Stephen Fairris <stephen@stelsey.com>
2019-07-23 11:08:52 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/views/index.html')))
+app.get('/contact', (req, res) => res.sendFile(path.join(__dirname + '/views/contact.html')))
+
+app.use(express.static('public'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
