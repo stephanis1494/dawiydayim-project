977b6a4819bedfd3c022ecbc72651738ee0d2272
Stephen Fairris <stephen@stelsey.com>
2019-07-27 15:56:58 -0700

    added about route and npm start script

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = 3000
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/views/index.html')))
 app.get('/contact', (req, res) => res.sendFile(path.join(__dirname + '/views/contact.html')))
+app.get('/about', (req, res) => res.sendFile(path.join(__dirname + '/views/about.html')))
 
 app.use(express.static('public'))
 
