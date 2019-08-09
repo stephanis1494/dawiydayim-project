f6e087dac5ce6688315ce7de8b6747a2bf15b8ef
dawiydayim <dawiyd@gmail.com>
2019-08-09 10:05:12 +0300

    added some responsive design for index. added religion html and css.

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const port = 3000
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/views/index.html')))
 app.get('/contact', (req, res) => res.sendFile(path.join(__dirname + '/views/contact.html')))
-
+app.get('/about', (req, res) => res.sendFile(path.join(__dirname + '/views/about.html')))
+app.get('/religion', (req, res) => res.sendFile(path.join(__dirname + '/views/religion.html')))
 app.use(express.static('public'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
